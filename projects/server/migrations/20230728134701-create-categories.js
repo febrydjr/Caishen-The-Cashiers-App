d2fbfa4c01207431695e76d3ba1a9d169d4358c7
Febry Dharmawan Junior <83161819+febrydjr@users.noreply.github.com>
2023-07-28 21:46:03 +0700

    models cat, prod_cat, prod (#1)
    * models cat, prod_cat, prod
    
    * uuidv4
    
    * config env

---
+++ projects/server/migrations/20230728134701-create-categories.js
@@ -0,0 +1,28 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+const { v4: uuidv4 } = require("uuid");
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("categories", {
+      id: {
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: uuidv4(),
+      },
+      name: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("categories");
+  },
+};
