ca320f29f7b82ff754eb1f0784901fd57250b103
Febry Dharmawan Junior <83161819+febrydjr@users.noreply.github.com>
2023-07-31 15:06:08 +0700

    Mp 11 mp 13 fe (#15)
    * loginpage
    
    * login FE

---
+++ projects/client/src/pages/Login.jsx
@@ -0,0 +1,175 @@
+import React from "react";
+import axios from "axios";
+import {
+  Box,
+  Input,
+  Button,
+  Link,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  Text,
+  useToast,
+} from "@chakra-ui/react";
+import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
+
+const Login = () => {
+  const [Toggle, setToggle] = React.useState(false);
+  const toast = useToast();
+  const validationSchema = Yup.object().shape({
+    identifier: Yup.string().required("Username/Email is required"),
+    password: Yup.string().required("Password is required"),
+    email: Yup.string().email("Invalid email").required("Email is required"),
+  });
+
+  const initialValues = {
+    identifier: "",
+    password: "",
+    email: "",
+  };
+
+  const handleReset = async (values) => {
+    console.log(values.email, "RESET PASSWORD");
+  };
+
+  const handleLogin = async (values) => {
+    try {
+      const { identifier, password } = values;
+      const res = await axios.post("http://localhost:8000/api/auth/login", {
+        identifier,
+        password,
+      });
+      if (res.status === 200) {
+        toast({
+          title: "Success",
+          description: "Login Success",
+          status: "success",
+          duration: 9000,
+          isClosable: true,
+        });
+      }
+    } catch (err) {
+      // console.error(err);
+      toast({
+        title: "Error",
+        description: "Login Failed",
+        status: "error",
+        duration: 9000,
+        isClosable: true,
+      });
+    }
+  };
+
+  return (
+    <Box
+      bgImage={"/bglogin.png"}
+      fontFamily={"Fira Code"}
+      backgroundSize={"cover"}
+      h={"100vh"}
+    >
+      <Box display="flex" alignItems="center" justifyContent="center" h="100vh">
+        <Box
+          bgColor={"#2A2B2E"}
+          w="450px"
+          p={5}
+          border={"1px solid #2D2D2D"}
+          borderWidth={1}
+          borderRadius={8}
+          color={"white"}
+          boxShadow={"dark-lg"}
+        >
+          <Text fontSize={"4xl"}>Caishen Login!</Text>
+          <Text fontSize="12px" mb={8}>
+            Welcome back! please enter your detail!
+          </Text>
+          <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={Toggle ? handleReset : handleLogin}
+          >
+            {({ errors, touched }) => (
+              <Form>
+                {!Toggle && (
+                  <>
+                    <Field name="identifier">
+                      {({ field, form }) => (
+                        <FormControl
+                          isInvalid={
+                            form.errors.identifier && form.touched.identifier
+                          }
+                          mb={2}
+                        >
+                          <FormLabel htmlFor="identifier">
+                            Username/Email
+                          </FormLabel>
+                          <Input {...field} id="identifier" />
+                          <FormErrorMessage>
+                            {form.errors.identifier}
+                          </FormErrorMessage>
+                        </FormControl>
+                      )}
+                    </Field>
+                    <Field name="password">
+                      {({ field, form }) => (
+                        <FormControl
+                          isInvalid={
+                            form.errors.password && form.touched.password
+                          }
+                          mb={2}
+                        >
+                          <FormLabel htmlFor="password">Password</FormLabel>
+                          <Input {...field} id="password" type="password" />
+                          <FormErrorMessage>
+                            {form.errors.password}
+                          </FormErrorMessage>
+                        </FormControl>
+                      )}
+                    </Field>
+                    <Button type="submit" colorScheme="gray" mt={1} mb={4}>
+                      Login
+                    </Button>
+                    <Text fontSize={"12px"}>
+                      Forgot Password? &nbsp;
+                      <Link onClick={() => setToggle(!Toggle)}>
+                        Click here!
+                      </Link>
+                    </Text>
+                  </>
+                )}
+                {Toggle && (
+                  <>
+                    <Field name="email">
+                      {({ field, form }) => (
+                        <FormControl
+                          isInvalid={form.errors.email && form.touched.email}
+                          mb={2}
+                        >
+                          <FormLabel htmlFor="email">Email</FormLabel>
+                          <Input {...field} id="email" type="email" />
+                          <FormErrorMessage>
+                            {form.errors.email}
+                          </FormErrorMessage>
+                        </FormControl>
+                      )}
+                    </Field>
+                    <Button type="submit" mb={4} colorScheme="blue">
+                      Send Link
+                    </Button>
+                    <Text fontSize={"12px"}>
+                      <Link onClick={() => setToggle(!Toggle)}>
+                        Back to login
+                      </Link>
+                    </Text>
+                  </>
+                )}
+              </Form>
+            )}
+          </Formik>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default Login;
--- projects/server/src/controllers/productController.js
+++ projects/server/src/controllers/productController.js
@@ -2,73 +2,70 @@ const { productService } = require("../services");
 const { messages } = require("../helpers");
 
 async function getProducts(req, res) {
-    try {
-        const result = await productService.getProducts(req);
-        res.status(result.status).json(messages.response(result));
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
+  try {
+    const result = await productService.getProducts(req);
+    res.status(result.status).json(messages.response(result));
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 }
 
 async function getProduct(req, res) {
-    try {
-        const { id } = req.params;
-        const result = await productService.getProduct(id);
-        res.status(result.status).json(messages.response(result));
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
+  try {
+    const { id } = req.params;
+    const result = await productService.getProduct(id);
+    res.status(result.status).json(messages.response(result));
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 }
 
 async function addProduct(req, res) {
-    try {
-        const result = await productService.addProduct(req);
-        res.status(result.status).json(messages.response(result));
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
+  try {
+    const result = await productService.addProduct(req);
+    res.status(result.status).json(messages.response(result));
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 }
 
 async function addCategory(req, res) {
-    try {
-        const { name } = req.body;
-        const newCategory = await productService.addCategory(name);
-        return res.status(201).json(messages.response(newCategory));
-    } catch (error) {
-        return res.status(500).json(error.message);
-    }
+  try {
+    const { name } = req.body;
+    const newCategory = await productService.addCategory(name);
+    return res.status(201).json(messages.response(newCategory));
+  } catch (error) {
+    return res.status(500).json(error.message);
+  }
 }
 
 async function editCategory(req, res) {
-    try {
-        const { categoryId, name } = req.body;
-        const updatedCategory = await productService.editCategory(
-            categoryId,
-            name
-        );
-        return res.status(200).json(messages.response(updatedCategory));
-    } catch (error) {
-        return res.status(500).json(error.message);
-    }
+  try {
+    const { categoryId, name } = req.body;
+    const updatedCategory = await productService.editCategory(categoryId, name);
+    return res.status(200).json(messages.response(updatedCategory));
+  } catch (error) {
+    return res.status(500).json(error.message);
+  }
 }
 
 async function deleteCategory(req, res) {
-    try {
-        const { categoryId } = req.params;
-        await productService.deleteCategory(categoryId);
-        return res
-            .status(200)
-            .json(messages.success("Category deleted successfully"));
-    } catch (error) {
-        return res.status(500).json(error.message);
-    }
+  try {
+    const { categoryId } = req.params;
+    await productService.deleteCategory(categoryId);
+    return res
+      .status(200)
+      .json(messages.success("Category deleted successfully"));
+  } catch (error) {
+    return res.status(500).json(error.message);
+  }
 }
 
 module.exports = {
-    addProduct,
-    getProduct,
-    getProducts,
-    addCategory,
-    editCategory,
-    deleteCategory,
+  addProduct,
+  getProduct,
+  getProducts,
+  addCategory,
+  editCategory,
+  deleteCategory,
 };
