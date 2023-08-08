54e8b47759eab1ee79b84f76ec76205f8293a989
febrydjr <febrydj99@gmail.com>
2023-08-08 20:51:28 +0700

    edit username, email cashier

--- projects/server/src/services/auth/index.js
+++ projects/server/src/services/auth/index.js
@@ -2,10 +2,14 @@ const login = require("./login");
 const register = require("./register");
 const forgotPassword = require("./forgotPassword");
 const resetPassword = require("./resetPassword");
+const changeUsername = require("./changeUsername");
+const changeEmail = require("./changeEmail");
 
-module.exports = { 
-    login, 
-    register, 
-    forgotPassword,
-    resetPassword,
+module.exports = {
+  login,
+  register,
+  forgotPassword,
+  resetPassword,
+  changeUsername,
+  changeEmail,
 };
